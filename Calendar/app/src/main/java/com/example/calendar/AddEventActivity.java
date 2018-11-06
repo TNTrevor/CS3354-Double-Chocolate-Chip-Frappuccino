f79fc44bcffce42f36628ba9138d93c631d875e8
Cythridarius <nrc170030@utdallas.edu>
2018-11-06 13:20:32 -0600

    Changes to main activity
    Plus button takes you from the main calendar activity to the add event activity.
    
    The date textfield is autofilled with the selected date.

---
+++ Calendar/app/src/main/java/com/example/calendar/AddEventActivity.java
@@ -0,0 +1,28 @@
+package com.example.calendar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class AddEventActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.add_event);
+
+        Intent intent = getIntent();
+        int selectedDay = intent.getIntExtra("day", 01);
+        int selectedYear = intent.getIntExtra("year", 2000);
+        int selectedMonth = intent.getIntExtra("month", 01);
+
+        String autoFillDate = (selectedMonth+ "/"+ selectedDay + "/" + selectedYear);
+        EditText dateText = findViewById(R.id.editText8);
+        dateText.setText(autoFillDate);
+    }
+}
--- Calendar/app/src/main/java/com/example/calendar/MainActivity.java
+++ Calendar/app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calendar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
@@ -22,20 +23,26 @@ public class MainActivity extends AppCompatActivity {
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 Toast.makeText(getApplicationContext(),month+ "/"+ dayOfMonth + "/" + year, Toast.LENGTH_SHORT).show();
                 System.out.println("YR: " + year + "MN: " + month);
-                //day = dayOfMonth;
+                day = dayOfMonth;
+                inputMonth = month;
+                inputYear = year;
             }
         });
 
-    }
 
-   /* Button plusButton = (Button) findViewById(R.id.angry_btn);
+        final Button plusButton = findViewById(R.id.addevent_btn);
         plusButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                System.out.println("FUCK");
+                Intent intent = new Intent(v.getContext(), AddEventActivity.class);
+                intent.putExtra("day", day);
+                intent.putExtra("month", inputMonth+1);
+                intent.putExtra("year", inputYear);
+                startActivity(intent);
             }
         });
+    }
+    private int day;
+    private int inputMonth;
+    private int inputYear;
 
-    public int day;
-    */
 }
